d3c78af4222fae75622e73ea8ef6ff17caed4bb9
Tim Caswell <tim@creationix.com>
2015-03-05 16:46:44 -0600

    Log some events

--- main.lua
+++ main.lua
@@ -1,9 +1,46 @@
 local glfw = require('glfw')
-p(glfw.glfwInit())
+
+assert(glfw.glfwInit() ~= 0, "Failed to initialize GLFW")
 
 local window = glfw.glfwCreateWindow(640, 480, "Hello World", nil, nil)
 
 glfw.glfwMakeContextCurrent(window)
+-- glfw.glfwSwapInterval(1)
+
+local function keyCallback(window, key, scancode, action, mods)
+  p{
+    type = "KEY",
+    window = window,
+    key = key,
+    scancode = scancode,
+    action = action,
+    mods = mods
+  }
+end
+jit.off(keyCallback)
+glfw.glfwSetKeyCallback(window, keyCallback)
+
+local function posCallback(window, x, y)
+  p{
+    type="POS",
+    window = window,
+    x = x,
+    y = y,
+  }
+end
+jit.off(posCallback)
+glfw.glfwSetWindowPosCallback(window, posCallback)
+
+local function sizeCallback(window, w, h)
+  p{
+    type="SIZE",
+    window=window,
+    w=w,
+    h=h
+  }
+end
+jit.off(sizeCallback)
+glfw.glfwSetWindowSizeCallback(window, sizeCallback)
 
 -- Loop until the user closes the window
 while glfw.glfwWindowShouldClose(window) == 0 do
@@ -13,8 +50,8 @@ while glfw.glfwWindowShouldClose(window) == 0 do
   -- Swap front and back buffers
   glfw.glfwSwapBuffers(window)
 
-  -- Poll for and process events
-  glfw.glfwPollEvents();
+  -- Wait for and process events
+  glfw.glfwWaitEvents();
 end
 
 glfw.glfwTerminate();
