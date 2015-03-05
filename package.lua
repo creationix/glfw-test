b9ce827eaef1bfa7abb65b6d09c601197811e3fd
Tim Caswell <tim@creationix.com>
2015-03-05 14:22:53 -0600

    Fix for bundle

--- package.lua
+++ package.lua
@@ -1,9 +1,14 @@
 return {
-  name = "glfw demo",
+  name = "glfw-demo",
   version = "0.0.0",
   private = true,
   dependencies = {
     "luvit/require",
     "luvit/pretty-print"
+  },
+  files = {
+    "libs/**",
+    "*.lua",
+    "native/$OS-$ARCH**",
   }
 }
